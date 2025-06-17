61483ae7659cb43643dc78c8bd321cd76b849fca
gouravsharma-00 <gourav.sharma.glamat2932@gmail.com>
2025-06-17 18:39:40 +0000

    dynamic

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-// export const dynamic = "force-dynamic"; // Error occurred prerendering page
+export const dynamic = "force-dynamic"; // Error occurred prerendering page
 import fetchAllFileContents from '@libs/gitlab/lab'
 
 import Link from "next/link";
